deed8f0bd380d06a149acbf854a71ab6df331cc8
MutaiAllan <Musalimoo19@gmail.com>
2024-02-14 20:24:31 +0300

    Added function for login

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,35 @@
 import { Link } from "react-router-dom";
+import React from "react"
+import { AffinidiLoginButton, useAffinidiProfile } from '@affinidi/affinidi-react-auth'
 
 function Header() {
+
+  const { isLoading, error, profile, handleLogout } = useAffinidiProfile({
+    authCompleteUrl: '/api/affinidi-auth/complete'
+ })
+
+ async function logout() {
+  handleLogout();
+  window.location.href = "/";
+}
+
   return (
     <header>
       <h1>
         <Link to="/">Lorem Blogum</Link>
       </h1>
+      
+      {!profile && <>
+        <AffinidiLoginButton />
+      </>}
+
+      {profile && <>
+          <button style={{ marginRight: 10 }} onClick={logout}>
+            Logout
+          </button>
+        </>}
+
+      {isLoading && <p>Loading...</p>}
     </header>
   );
 }
