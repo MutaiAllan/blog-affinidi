45fa5de173faae8e7eef8d3f1bfb19826c79c8ea
MutaiAllan <allankiprop175@gmail.com>
2024-02-18 23:28:00 +0300

    Updated configuration

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Header from "./components/Header";
+import UserContext from './context/UserContext';
+import React, {useState} from 'react';
+
 
 function App() {
+  const [userProfile, setUserProfile] = useState(null);
+
   return (
+    <UserContext.Provider value={{ profile: userProfile, setProfile: setUserProfile }}>
     <Router>
       <div className="App">
         <Header />
@@ -15,6 +21,7 @@ function App() {
         </Switch>
       </div>
     </Router>
+    </UserContext.Provider>
   );
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,58 @@
 import { Link } from "react-router-dom";
-import React from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { AffinidiLoginButton, useAffinidiProfile } from '@affinidi/affinidi-react-auth'
+import UserContext from '../context/UserContext';
 
 function Header() {
+
+  const { setProfile } = useContext(UserContext);
 
   const { isLoading, error, profile, handleLogout } = useAffinidiProfile({
     authCompleteUrl: '/api/affinidi-auth/complete'
  })
+
+ const [localProfile, setLocalProfile] = useState(null);
+
+ useEffect(() => {
+  // Convert objects to strings to compare them
+  const currentProfileStr = JSON.stringify(profile);
+  const localProfileStr = JSON.stringify(localProfile);
+
+  // Only update if the stringified versions differ
+  if (currentProfileStr !== localProfileStr) {
+    setLocalProfile(profile);
+    setProfile(profile); // assuming setProfile comes from a context and is stable
+  }
+}, [profile])
+
+const renderLoginState = () => {
+  console.log(profile)
+  if (isLoading) {
+    console.log(profile)
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    console.log(profile)
+    handleLogout();
+    return (
+      <div>
+        <p>Unable to load user data. Please try again later.</p>
+      </div>
+    );
+  }
+
+  if (profile) {
+      return (
+        <div>
+          <span>Welcome, {profile.givenName}</span>
+          <button onClick={logout}>Logout</button>
+        </div>
+      );
+    }
+
+return <AffinidiLoginButton />;
+  };
 
  async function logout() {
   handleLogout();
@@ -19,15 +65,13 @@ function Header() {
         <Link to="/">Articly</Link>
       </h1>
       
-      {!profile && <>
-        <AffinidiLoginButton />
-      </>}
+      {renderLoginState()}
 
-      {profile && <>
+      {/* {profile && <>
           <button style={{ marginRight: 10 }} onClick={logout}>
             Logout
           </button>
-        </>}
+        </>} */}
 
       {isLoading && <p>Loading...</p>}
     </header>
